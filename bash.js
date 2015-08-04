dcddaff8c6d2b3f88446623022cd0325a3bac814
Daniel Moennich <daniel.moennich@gmail.com>
2015-08-04 13:09:41 -0400

    Added support for pipes.

--- bash.js
+++ bash.js
@@ -3,22 +3,47 @@ var cmds = require("./command.js");
 
 process.stdout.write("prompt> ");
 
-process.stdin.on("data", function(data){
-	var inputs = data.toString().trim().split(" ");
-	// echo "hello"
-	// ls /Users/test
+var cmdList;
+
+var callNextCmd = function(stdin){
+	var inputs = cmdList.shift().split(" ");
+
 	var cmd = inputs[0];
 	var args = inputs.slice(1);
 	args.unshift(done);
+	args.unshift(stdin);
 
 	if(cmds[cmd]){
 		cmds[cmd].apply(this, args);
 	}
+};
+
+
+process.stdin.on("data", function(data){
+	//var inputs = data.toString().trim().split(" ");
+
+	var cmdString = data.toString().trim();
+	cmdList = cmdString.split(/\s*\|\s*/g);
+
+	callNextCmd();
+	// var inputs = cmdList.pop().split(" ");
+
+	// var cmd = inputs[0];
+	// var args = inputs.slice(1);
+	// args.unshift(done);
+
+	// if(cmds[cmd]){
+	// 	cmds[cmd].apply(this, args);
+	// }
 
 });
 
 
 var done = function(output){
-	console.log(output);
-	process.stdout.write("prompt> ");
+	if(cmdList.length > 0){
+		callNextCmd(output);
+	}else {
+		console.log(output);
+		process.stdout.write("prompt> ");
+	}
 };
--- command.js
+++ command.js
@@ -5,16 +5,16 @@ var request = require("request");
 
 var commands = {
 
-	date: function(callback){
+	date: function(stdin, callback){
 		callback(new Date());
 	},
 
 
-	pwd: function(callback){
+	pwd: function(stdin, callback){
 		callback(process.cwd());
 	},
 
-	ls: function(callback){
+	ls: function(stdin, callback){
 		fs.readdir('.', function(err, files) {
 			if (err) throw err;
 			var output = "";
@@ -25,50 +25,114 @@ var commands = {
 		});
 	},
 
-	echo: function(callback, file){
+	echo: function(stdin, callback, file){
 		callback(file);
 	},
 
-	cat: function(callback, file){
+	cat: function(stdin, callback, file){
 		//sync solution: var fileContent = fs.readFileSync(file, "utf8");
 		fs.readFile(file, "utf8", function(err, data){
 			callback(data);
 		});
 	},
 
-	head: function(callback, file){
-		fs.readFile(file, "utf8", function(err, data){
-			callback(data.split("\n").slice(0, 5).join("\n"));
+	head: function(stdin, callback, file){
+
+		innerHelper(stdin, callback, file, function(input){
+			return input.split("\n").slice(0, 5).join("\n");
 		});
+
+
+		// var innerFunc = function(input){
+		// 	callback(input.split("\n").slice(0, 5).join("\n"));
+		// };
+
+		// if(stdin){
+		// 	innerFunc(stdin);
+		// }else {
+		// 	fs.readFile(file, "utf8", function(err, data){
+		// 		innerFunc(data);
+		// 	});
+		// }
+
+
 	},
 
-	tail: function(callback, file){
-		fs.readFile(file, "utf8", function(err, data){
-			callback(data.split("\n").slice(-5).join("\n"));
+	tail: function(stdin, callback, file){
+
+
+		innerHelper(stdin, callback, file, function(input){
+			return input.split("\n").slice(-5).join("\n");
 		});
+
+
+		// var innerFunc = function(input){
+		// 	callback(input.split("\n").slice(-5).join("\n"));
+		// };
+
+		// if(stdin){
+		// 	innerFunc(stdin);
+		// }else {
+		// 	fs.readFile(file, "utf8", function(err, data){
+		// 		innerFunc(data);
+		// 	});
+		// }
+
 	},
 
-	sort: function(callback, file){
-		fs.readFile(file, "utf8", function(err, data){
-			callback(data.split("\n").sort().join("\n"));
+	sort: function(stdin, callback, file){
+
+
+		innerHelper(stdin, callback, file, function(input){
+			return input.split("\n").sort().join("\n");
 		});
+
+		// var innerFunc = function(input){
+		// 	callback(input.split("\n").sort().join("\n"));
+		// };
+
+		// if(stdin){
+		// 	innerFunc(stdin);
+		// }else {
+		// 	fs.readFile(file, "utf8", function(err, data){
+		// 		innerFunc(data);
+		// 	});
+		// }
+
 	},
 
-	wc: function(callback, file){
-		fs.readFile(file, "utf8", function(err, data){
-			callback(data.split("\n").length);
+	wc: function(stdin, callback, file){
+
+
+		innerHelper(stdin, callback, file, function(input){
+			return input.split("\n").length;
 		});
+
+		// var wcFunc = function(input){
+		// 	callback(input.split("\n").length);
+		// };
+
+		// if(stdin){
+		// 	wcFunc(stdin);
+		// }else {
+		// 	fs.readFile(file, "utf8", function(err, data){
+		// 		wcFunc(data);
+		// 	});
+		// }
+
 	},
 
-	uniq: function(callback, file){
-		fs.readFile(file, "utf8", function(err, data){
-			callback(data.split("\n").filter(function(line, index, self){
+	uniq: function(stdin, callback, file){
+
+		innerHelper(stdin, callback, file, function(input){
+			return input.split("\n").filter(function(line, index, self){
 				return self.indexOf(line) === index;
-			}).join("\n"));
+			}).join("\n");
 		});
+
 	},
 
-	curl: function(callback, url){
+	curl: function(stdin, callback, url){
 		request(url, function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				callback(body);
@@ -80,6 +144,22 @@ var commands = {
 
 };
 
+
+var innerHelper = function(stdin, callback, file, func){
+
+		var innerFunc = function(input){
+			callback(func(input));
+		};
+
+		if(stdin){
+			innerFunc(stdin);
+		}else {
+			fs.readFile(file, "utf8", function(err, data){
+				innerFunc(data);
+			});
+		}
+		
+};
 
 
 
